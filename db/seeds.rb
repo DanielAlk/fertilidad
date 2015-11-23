3ab0a499c1a522dbf934eb0cfd3cc0d403695fc2
DanielAlk <daniodfad@yahoo.com.ar>
2015-11-23 11:28:12 -0300

    delete my user admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Admin.create(email: 'admin@lab.com', password: '12345678', profile: 'administrator')
-Admin.create(email: 'daniognr@hotmail.com', password: 'passwo00', profile: 'administrator')
 
 Schedule.create(title: 'DÍAS Y HORARIOS DE ATENCIÓN SIN TURNO', text: 'Martes de 08:00 a 16:00 hs., Miércoles de 10:30 a 19:00 hs.<br>Jueves de 10:30 a 16:00 hs., Viernes de 10:30 a 16:00 hs.')
 Schedule.create(title: 'OTROS DÍAS/ HORARIOS CON TURNO', text: 'Solicitar turno al +54 221 4219364 o al +54 221 15 4857016<br>o via mail <a href="mailto:info@laboratorioyfertilidad.com">info@laboratorioyfertilidad.com</a>')
